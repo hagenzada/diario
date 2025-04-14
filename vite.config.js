afc00e0efae9ba0f494bc50f20430d1d98442285
hagenzada <juliana.hagen.1991@gmail.com>
2025-04-13 22:29:57 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  root: '.', // raiz correta do projeto
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      input: path.resolve(__dirname, 'index.html'),
-    },
   },
 });
